9cf3cd0698a729efe64233e525c022981dd5adc2
aditaryar <adityarya189@gmail.com>
2025-05-14 21:36:40 +0700

    version 0.1

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: const Size(360, 690), // <- ukuran desain dasar kamu
+      designSize: const Size(360, 690), 
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (context, child) {
